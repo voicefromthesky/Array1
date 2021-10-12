5f69a5c912d93e5257bb850f34e5e60e477ff87e
voicefromthesky <andrewdoverin@gmail.com>
2021-10-12 20:53:00 +0300

    first commit

---
+++ Arrays1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Arrays1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 100000;
+            int index = 0;
+            int[] array = new int[length];
+            
+            while (true)
+            {
+                //length++;
+                Console.WriteLine("write your number or 'stop' if you want to stop");
+                var input = Console.ReadLine();
+                if (input == "stop")
+                {
+                    break;
+                }
+               if(input != "stop")
+                {
+                    
+                    int number = int.Parse(input);
+                    array[index] = number;
+                   
+                    index++;
+
+
+                }
+            }
+
+            for (int i = 0; i < index; i++)
+                Console.WriteLine(array[i]);
+            {
+
+            }
+            
+
+            
+            
+            
+        }
+    }
+}
